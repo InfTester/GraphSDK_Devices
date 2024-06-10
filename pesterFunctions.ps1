5264a5f0198855cbc637659ad43c07b240b019bf
InfTester <58252659+InfTester@users.noreply.github.com>
2024-06-10 22:30:35 +0100

    Update pesterFunctions.ps1

--- pesterFunctions.ps1
+++ pesterFunctions.ps1
@@ -1,26 +1,217 @@
-function user-Details {
-<#
-Creator - Tony Law
-Date - 06/06/2024
-Version 0.1
+Function Out-PieChart {
+    <#
+.EXAMPLES
+#    3D pie chart
+Get-Process | select -First 5 name, pm | Out-PieChart -PieChartTitle "Top 5 Windows processes running" -DisplayToScreen -Pie3D
+
+#    standard pie chart
+Get-Process | select -First 5 name, pm | Out-PieChart -PieChartTitle "Top 5 Windows processes running" -DisplayToScreen
+Display pie chart to screen
+#use switch -DisplayToScreen
+
+Get-Process | select -First 5 name, pm | Out-PieChart -PieChartTitle "Top 5 Windows processes running" -DisplayToScreen
+
+     #Save pie chart
+Get-Process | select -First 5 name, pm | Out-PieChart -PieChartTitle "Top 5 Windows processes running" -saveImage 'E:\OneDrive\Scripting\PesterScripts\Win_Process.png'
 #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [psobject] $inputObject,
+        [Parameter()]
+        [string] $PieChartTitle,
+        [Parameter()]
+        [int] $ChartWidth = 1000,
+        [Parameter()]
+        [int] $ChartHeight = 475,
+        [Parameter()]
+        [string[]] $NameProperty,
+        [Parameter()]
+        [string] $ValueProperty,
+        [Parameter()]
+        [switch] $Pie3D,
+        [Parameter()]
+        [switch] $DisplayToScreen,
+        [Parameter()]
+        [string] $saveImage
+    )
+    begin {
+        Add-Type -AssemblyName System.Windows.Forms.DataVisualization
+        # Frame
+        $Chart = [System.Windows.Forms.DataVisualization.Charting.Chart]@{
+            Width       = $ChartWidth
+            Height      = $ChartHeight
+            BackColor   = 'White'
+            BorderColor = 'Black'
+        }
+        # Body
+        $null = $Chart.Titles.Add($PieChartTitle)
+        $Chart.Titles[0].Font = "segoeuilight,20pt"
+        $Chart.Titles[0].Alignment = "TopCenter"
+        # Create Chart Area
+        $ChartArea = [System.Windows.Forms.DataVisualization.Charting.ChartArea]::new()
+        $ChartArea.Area3DStyle.Enable3D = $Pie3D.ToBool()
+        $ChartArea.Area3DStyle.Inclination = 50
+        $Chart.ChartAreas.Add($ChartArea)
+        # Define Chart Area
+        $null = $Chart.Series.Add("Data")
+        $Chart.Series["Data"].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
+        # Chart style
+        $Chart.Series["Data"]["PieLabelStyle"] = "Outside"
+        $Chart.Series["Data"]["PieLineColor"] = "Black"
+        $Chart.Series["Data"]["PieDrawingStyle"] = "Concave"
+        $chart.Series["Data"].IsValueShownAsLabel = $true
+        $chart.series["Data"].Label = "#PERCENT\n#VALX"
+        # Set ArrayList
+        $XColumn = [System.Collections.ArrayList]::new()
+        $yColumn = [System.Collections.ArrayList]::new()
+    }
+    process {
+        if (-not $valueProperty) {
+            $numericProperties = foreach ($property in $inputObject.PSObject.Properties) {
+                if ([Double]::TryParse($property.Value, [Ref]$null)) {
+                    $property.Name
+                }
+            }
+            if (@($numericProperties).Count -eq 1) {
+                $valueProperty = $numericProperties
+            }
+            else {
+                throw 'Unable to automatically determine properties to graph'
+            }
+        }
+        if (-not $LabelProperty) {
+            if ($inputObject.PSObject.Properties.Count -eq 2) {
+                $LabelProperty = $inputObject.Properties.Name -ne $valueProperty
+            }
+            elseif ($inputObject.PSObject.Properties.Item('Name')) {
+                $LabelProperty = 'Name'
+            }
+            else {
+                throw 'Cannot convert Data'
+            }
+        }
+        # Bind chart columns
+        $null = $yColumn.Add($InputObject.$valueProperty)
+        $null = $xColumn.Add($inputObject.$LabelProperty)
+    }
+    end {
+        # Add data to chart
+        $Chart.Series["Data"].Points.DataBindXY($xColumn, $yColumn)
+        # Save file
+        if ($psboundparameters.ContainsKey('saveImage')) {
+            try{
+                if (Test-Path (Split-Path $saveImage -Parent)) {
+                    $SaveImage = $pscmdlet.GetUnresolvedProviderPathFromPSPath($saveImage)
+                    $Chart.SaveImage($saveImage, "png")
+                } else {
+                    throw 'Invalid path, the parent directory must exist'
+                }
+            } catch {
+                throw
+            }
+        }
+        # Display Chart to screen
+        if ($DisplayToScreen.ToBool()) {
+            $Form = [Windows.Forms.Form]@{
+                Width           = 800
+                Height          = 450
+                AutoSize        = $true
+                FormBorderStyle = "FixedDialog"
+                MaximizeBox     = $false
+                MinimizeBox     = $false
+                KeyPreview      = $true
+            }
+            $Form.controls.add($Chart)
+            $Chart.Anchor = 'Bottom, Right, Top, Left'
+            $Form.Add_KeyDown({
+                if ($_.KeyCode -eq "Escape") { $Form.Close() }
+            })
+            $Form.Add_Shown( {$Form.Activate()})
+            $Form.ShowDialog() | Out-Null
+        }
+    }
+}
+
+#CSS codes
+$header = @"
+<style>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #e68a00;
+        font-size: 36px;
+    }
+    
+    h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #000099;
+        font-size: 24px;
+    }
+      
+   table {
+		font-size: 16px;
+		border: 0px; 
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+	
+    td {
+		padding: 4px;
+		margin: 0px;
+		border: 0;
+	}
+	
+    th {
+        background: #395870;
+        background: linear-gradient(#49708f, #293f50);
+        color: #fff;
+        font-size: 11px;
+        text-transform: uppercase;
+        padding: 10px 15px;
+        vertical-align: middle;
+	}
+
+    tbody tr:nth-child(even) {
+        background: #f0f0f2;
+    }
+  
+    #CreationDate {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #ff3300;
+        font-size: 12px;
+    }
+
+    .StopStatus {
+        color: #ff0000;
+    }
+  
+    .RunningStatus {
+        color: #008000;
+    }
+
+</style>
+"@
+
+function Get-userDetails {
+
 clear-host
 Write-Host "======================REQUIRED TEST EXECUTION DETAILS================================"
 Write-Host "`nHello $env:USERNAME - Please provide the following details, so they can be added to the report"
   do {
-    Read-Host "`nPlesae enter your First Name or initial" -OutVariable firstName
+    Read-Host "`nPlease enter your First Name or initial" -OutVariable firstName
   }
-  while ($firstName Length -lt 1) 
+  while ($firstName.Length -lt 1) 
 
   do {
-      Read-Host "`nPlesae enter your Last Name (minimum of 2 characters)" -OutVariable lastName
+    Read-Host "`nPlease enter your Last Name '(minimum of 2 characters)'" -OutVariable lastName
   }
-  while ($firstName Length -lt 2)
+  while ($lastName.Length -lt 2)
 
-  do (
-      Read-Host "`nPlesae enter your Team Name (minimum of 2 characters)" -OutVariable teamName
+  do {
+    Read-Host "`nPlease enter your Team Name '(minimum of 2 characters)'" -OutVariable teamName
   }
-  while ($teamName Length -lt 2)
+  while ($teamName.Length -lt 2)
+ 
 Write-Host "`nSubmitting Data ...."
 Start-Sleep -seconds 1
 Write-Host $firstName
@@ -30,7 +221,7 @@ Start-Sleep -seconds 1
 Write-Host $teamName
 Start-Sleep -seconds 1
 Write-Host "`nValidating ........."
-Start-Sleep -seconds
+Start-Sleep -seconds 3
 Write-Host $firstname';' $lastName';' $teamName
 Start-Sleep -seconds 3
 Write-Host "`nInput Accepted, validation complete"
@@ -39,7 +230,7 @@ Write-Host "`n$firstName $lastName of the $teamName Team will be added to the fi
 Pause
 }
 
-function User-Agreement {
+function Out-userAgreement {
 <#
 Creator - Tony Law
 Date - 06/06/2024
@@ -56,13 +247,13 @@ Start-Sleep -Seconds 2.5
 Pause
 }
 
-}
 
-function mgAccess-MgSDK {
+function start-MgSDK {
 <#
 Creator - Tony Law
 Date - 06/06/2024
 Version 0.1
+Assumptions: MSGraphAPI module is installed on the device this is being run from. If not, this function will fail.
 #>
         Write-Host "`n`n$env:USERNAME, You are now connecting to the Azure Tenant using Microsoft`n" 
         Start-Sleep -Seconds 1
@@ -77,7 +268,6 @@ Version 0.1
         }
         Start-Sleep -Seconds 1
 }
-
 
 function mgWindows-UserDevice {
 <#
@@ -105,14 +295,12 @@ do {
 } While ($deviceID -notin ($devDetails).SerialNumber)
 $windowsOSDevice = Get-MgDeviceManagementManagedDevice | Where-Object {$_.SerialNumber -eq $deviceID}
 }
-
 function mgAndroid-UserDevice {
 <#
 Creator - Tony Law
 Date - 06/06/2024
 Version 0.1
-#>
-        
+#>   
         $devDetails = Get-MgDeviceManagementManagedDevice | Where-Object {$_.OperatingSystem -eq "android"}
         Write-Host "`nCollecting Users...."
         $userDispName = Get-MgDeviceManagementManagedDevice | Where-Object {$_.OperatingSystem -eq "android"} | Select-Object UserDisplayName -Unique 
@@ -133,7 +321,6 @@ Version 0.1
         } While ($deviceID -notin ($devDetails).SerialNumber)
         $androidDevice = Get-MgDeviceManagementManagedDevice | Where-Object {$_.SerialNumber -eq $deviceID}
 }
-
 
 function mgIoS-UserDevice {
 <#
@@ -161,7 +348,6 @@ do {
 } While ($deviceID -notin ($devDetails).SerialNumber)
 $androidDevice = Get-MgDeviceManagementManagedDevice | Where-Object {$_.SerialNumber -eq $deviceID}
 }
-
 function mgMacOS-UserDevice {
 <#
 Creator - Tony Law
@@ -187,8 +373,6 @@ do {
     $deviceID = Read-Host -Prompt "Copy and paste the 'Serial Number' from above and then press enter to execute SIT"
 } While ($deviceID -notin ($devDetails).SerialNumber)
 $androidDevice = Get-MgDeviceManagementManagedDevice | Where-Object {$_.SerialNumber -eq $deviceID}
-}
-
 function mgLinux-UserDevice {
 <#
 Creator - Tony Law
@@ -215,3 +399,39 @@ do {
 } While ($deviceID -notin ($devDetails).SerialNumber)
 $androidDevice = Get-MgDeviceManagementManagedDevice | Where-Object {$_.SerialNumber -eq $deviceID}
 }
+
+function add-HtmlImage {
+    Param(
+      [Switch]
+      [bool]$IncludeImages
+    )
+  
+    if ($IncludeImages) {
+      $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+      $images = Get-ChildItem "$PSScriptRoot\*.png"
+      $ImageHTML = $images | % {
+        $ImageBits = [Convert]::ToBase64String((Get-Content $_ -Encoding Byte))
+        "<img src=data:image/png;base64,$($ImageBits) alt='My Image'/>"
+      }
+  
+      ConvertTo-Html -Body $style -PreContent $imageHTML |
+        Out-File "$PSScriptRoot\report.html"
+    }
+  }
+
+  function add-HtmlImages {
+    Param(
+      [Parameter()]
+      [string[]]$IncludeImages
+    )
+  
+    if ($IncludeImages) {
+      $ImageHTML = $IncludeImages | % {
+        $ImageBits = [Convert]::ToBase64String((Get-Content $_ -Encoding Byte))
+        "<img src=data:image/png;base64,$($ImageBits) alt='My Image'/>"
+      }
+  
+      ConvertTo-Html -Body $style -PreContent $imageHTML |
+        Out-File "C:\path\to\report.html"
+    }
+  }
